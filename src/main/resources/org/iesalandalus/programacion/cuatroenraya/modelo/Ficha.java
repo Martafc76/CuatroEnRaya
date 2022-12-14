08b85df2ae5e62803605287346c0b4ef162d4bf9
Martafc76 <mfcgalu@gmail.com>
2022-12-14 14:18:34 +0100

    he cambiado el enumerado

--- src/main/resources/org/iesalandalus/programacion/cuatroenraya/modelo/Ficha.java
+++ src/main/resources/org/iesalandalus/programacion/cuatroenraya/modelo/Ficha.java
@@ -1,19 +1,8 @@
 package org.iesalandalus.programacion.cuatroenraya.modelo;
 
 public enum Ficha {
-	AZUL("azul"),
-	VERDE("verde");
-	
-	private String cadenaAMostrar;
-
-	private Ficha(String cadenaMostrar) {
-		this.cadenaAMostrar = cadenaMostrar;
-	}
-	//sobreescribe métodos
-	@Override
-	public String toString(){
-		return cadenaAMostrar;
-	}
+	AZUL,
+	VERDE;
 }
 
 
