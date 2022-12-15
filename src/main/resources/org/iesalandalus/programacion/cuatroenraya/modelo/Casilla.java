f1f634433f4a8a9c076745db5dd28c9e4ac12792
Martafc76 <mfcgalu@gmail.com>
2022-12-15 11:03:28 +0100

    He pasado el test de casilla

--- src/main/resources/org/iesalandalus/programacion/cuatroenraya/modelo/Casilla.java
+++ src/main/resources/org/iesalandalus/programacion/cuatroenraya/modelo/Casilla.java
@@ -1,10 +1,12 @@
 package org.iesalandalus.programacion.cuatroenraya.modelo;
 
+import javax.naming.OperationNotSupportedException;
+
 public class Casilla {
 	public Ficha ficha;
 	
 	public Casilla() {
-		setFicha(null);
+		ficha = null;
 	}
 
 	public boolean estaOcupada() {
@@ -21,10 +23,13 @@ public class Casilla {
 		return ficha;
 	}
 
-	public void setFicha(Ficha ficha) {
-		if (ficha == null) {
-			throw new NullPointerException("La ficha no puede ser nula");
+	public void setFicha(Ficha ficha) throws OperationNotSupportedException {
+		if(estaOcupada()) {
+			throw new OperationNotSupportedException("ERROR: Ya contengo una ficha.");
 		}
+		if(ficha == null) {
+			throw new NullPointerException("ERROR: No se puede poner una ficha nula.");
+			}
 		this.ficha = ficha;
 	}
 
--- src/main/resources/org/iesalandalus/programacion/cuatroenraya/modelo/Jugador.java
+++ src/main/resources/org/iesalandalus/programacion/cuatroenraya/modelo/Jugador.java
@@ -1,5 +1,34 @@
 package org.iesalandalus.programacion.cuatroenraya.modelo;
 
+import org.apache.commons.math3.exception.NullArgumentException;
+
 public class Jugador {
+	private String nombre;
+	private Ficha colorFicha;
+	
+	public Jugador(String nombre, Ficha colorFicha) {
+		setNombre(nombre);
+		setFicha(colorFicha);
+	}
+	
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	private void setNombre(String nombre) {
+		if(nombre == null) {
+			throw new NullPointerException("El nombre no puede ser nulo");
+		}
+		this.nombre = nombre;
+	}
+	public Ficha getFicha() {
+		return colorFicha;
+	}
+	private void setFicha(Ficha colorFicha) {
+		this.colorFicha = ficha;
+	}
+	
+	
 
 }
