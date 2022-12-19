2c3c8ea87fc7661bd6ec92ca4949b85f1caff5c5
Martafc76 <mfcgalu@gmail.com>
2022-12-19 10:28:24 +0100

    Para cambiar pc

---
+++ src/main/resources/Tablero.java
@@ -0,0 +1,16 @@
+import org.iesalandalus.programacion.cuatroenraya.modelo.Casilla;
+
+public class Tablero {
+	public static final int FILAS = 6;
+	public static final int COLUMNAS = 7;
+	public static final int FICHAS_IGUALES_CONSECUTIVAS_NECESARIAS = 4;
+	private Casilla[][] tablero;
+	
+	public Tablero(){
+		tablero = new Casilla[FILAS][COLUMNAS]
+		
+	}
+	
+	
+
+}
